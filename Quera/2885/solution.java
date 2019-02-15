ed039eee0833cbb0d44f47de9d1753550670770a
Mahdi Banisharif <41099498+BanisharifM@users.noreply.github.com>
2019-02-15 17:42:23 +0330

    Create solution.java

---
+++ Quera/2885/solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Q2App 
+{
+    public static void main(String[] args) 
+    {
+      Scanner input=new Scanner(System.in);
+      int n=input.nextInt();
+      for(int i=0;i<n;i++)
+          System.out.println("man khoshghlab hastam");
+    }
+    
+}
